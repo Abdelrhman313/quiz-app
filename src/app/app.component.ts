de046edc1ef6a6cfb4bf9b88c47cc47d288f9057
Amira-123 <70484629+Amira-123@users.noreply.github.com>
2022-06-21 17:23:26 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { AuthService } from './auth/services/auth.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private authservice:AuthService){}
   title = 'mcq-App';
+  ngOnInit(): void {
+    this.getUserData()
+  }
+  getUserData(){
+      this.authservice.getRole().subscribe((res:any)=>{
+      this.authservice.user.next(res)
+      },
+    (error)=>{
+      alert(error)
+    })
+  }
+
 }
